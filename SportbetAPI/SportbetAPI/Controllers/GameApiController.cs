448d622e9efe93e895eb6b0fa1583408f6488bd1
tomyka <tomyka@gmail.com>
2022-08-09 23:33:29 +0300

    added repo

---
+++ SportbetAPI/SportbetAPI/Controllers/GameApiController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportbetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameApiController : ControllerBase
+    {
+        private static List<Game> _games = new List<Game>();
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            _games.Add(game);
+
+            return Created("/game", game);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_games);
+        }
+
+        [HttpGet(template: "{gameId}")]
+        public IActionResult Get(int gameId)
+        {
+            var foundGame = findGame(gameId);
+            if (foundGame == null)
+            {
+                return NotFound($"Game with gameId {gameId} has not been found");
+            }
+            return Ok(foundGame);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            _games.Clear();
+            return Ok();
+        }
+
+        [HttpDelete("{gameId}")]
+        public IActionResult Delete(int gameId)
+        {
+            var foundGame = findGame(gameId);
+            if (foundGame == null)
+            {
+                return NotFound($"Game with gameId {gameId} has not been found");
+            }
+            _games.Remove(foundGame);
+            return Ok();
+        }
+
+        private Game findGame(int gameId)
+        {
+            foreach (var game in _games)
+            {
+                if (game.gameId == gameId)
+                {
+                    return game;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ SportbetAPI/SportbetAPI/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportbetAPI
+{
+    public class Game
+    {
+        public int gameId {get; set;}
+        public string gameDate { get; set; }
+        public string homeTeam { get; set; }
+        public string awayTeam { get; set; }
+        public int homeScore { get; set; }
+        public int awayScore { get; set; }
+    }
+}
